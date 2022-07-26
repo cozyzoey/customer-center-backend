8b7d5290e3527bbacc1e72b708402310dc7ea780
cozyzoey <devzoeykim@gmail.com>
2022-07-26 14:27:50 +0900

    feat: 쿠키 관련 초기작업

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,28 @@
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: env('AWS_BUCKET'),
+          Bucket: env("AWS_BUCKET"),
         },
       },
       actionOptions: {
-        upload: {ACL: null},
-        uploadStream: {ACL: null},
+        upload: { ACL: null },
+        uploadStream: { ACL: null },
         delete: {},
       },
     },
+    "users-permissions": {
+      config: {
+        jwt: {
+          expiresIn: "30d",
+        },
+      },
+    },
   },
-  ckeditor: true
-})
+  ckeditor: true,
+});
