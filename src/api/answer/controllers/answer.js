1c7c4720a75103cf509c51531f52cdaf7067525a
cozyzoey <devzoeykim@gmail.com>
2022-07-27 12:15:57 +0900

    feat: answer 수정/삭제

--- src/api/answer/controllers/answer.js
+++ src/api/answer/controllers/answer.js
@@ -1,9 +1,60 @@
-'use strict';
+"use strict";
 
 /**
  *  answer controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::answer.answer');
+module.exports = createCoreController("api::answer.answer", ({ strapi }) => ({
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const answer = await strapi.db.query("api::answer.answer").findOne({
+      where: {
+        id: ctx.params.id,
+        user: { id: ctx.state.user.id },
+      },
+    });
+
+    if (!answer) {
+      return ctx.unauthorized(`댓글 작성자가 아닙니다`);
+    }
+
+    entity = await strapi.entityService.update(
+      "api::answer.answer",
+      id,
+      ctx.request.body
+    );
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const answer = await strapi.db.query("api::answer.answer").findOne({
+      where: {
+        id: ctx.params.id,
+        user: { id: ctx.state.user.id },
+      },
+      populate: true,
+    });
+
+    if (!answer) {
+      return ctx.unauthorized(`댓글 작성자가 아닙니다`);
+    }
+
+    entity = await strapi.entityService.delete("api::answer.answer", id);
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
