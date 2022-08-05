57d9ec7c55a23d4171bc2c65ca5a5990e09bc5ad
cozyzoey <devzoeykim@gmail.com>
2022-08-05 09:18:35 +0900

    feat: sentry 추가 (에러 로거)

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "admin"),
+      user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
       charset: "utf8mb4",
       collation: "utf8mb4_unicode_ci",
