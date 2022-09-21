2496e99e05536f6778ee16765de359b74196b2c1
cozyzoey <devzoeykim@gmail.com>
2022-09-21 13:48:42 +0900

    feat: add deletedAt field to student, handle student update

--- src/api/student/controllers/student.js
+++ src/api/student/controllers/student.js
@@ -42,4 +42,84 @@ module.exports = createCoreController("api::student.student", ({ strapi }) => ({
 
     return { data, meta };
   },
+
+  async update(ctx) {
+    const { dataCollectionSession, deletedAt } = ctx.request.body.data;
+    const { id } = ctx.params;
+
+    /**
+     * 기존 저장된 데이터 정보
+     */
+    const prevStudentData = await strapi.db
+      .query("api::student.student")
+      .findOne({
+        where: {
+          id,
+        },
+        populate: true,
+      });
+
+    if (!prevStudentData) {
+      return ctx.badRequest();
+    }
+
+    if (prevStudentData.dataCollectionSession) {
+      const prevDataCollectionSession = await strapi.db
+        .query("api::data-collection-session.data-collection-session")
+        .findOne({
+          where: {
+            id: prevStudentData.dataCollectionSession.id,
+          },
+        });
+
+      // 기존 데이터 수집 세션의 남은 자리 + 1
+      await strapi.entityService.update(
+        "api::data-collection-session.data-collection-session",
+        prevDataCollectionSession.id,
+        {
+          data: {
+            remainingApplicants:
+              prevDataCollectionSession.remainingApplicants + 1,
+          },
+        }
+      );
+    }
+
+    /**
+     * 신청취소 요청이 아니고 일반 업데이트인 경우
+     */
+    if (!deletedAt) {
+      const curDataCollectionSession = await strapi.db
+        .query("api::data-collection-session.data-collection-session")
+        .findOne({
+          where: {
+            id: dataCollectionSession,
+          },
+        });
+
+      if (
+        !curDataCollectionSession ||
+        curDataCollectionSession.remainingApplicants === 0
+      ) {
+        return ctx.badRequest(`다른 데이터 수집 기간을 선택하세요`);
+      }
+
+      // 데이터 수집 세션의 남은 자리 - 1
+      await strapi.entityService.update(
+        "api::data-collection-session.data-collection-session",
+        dataCollectionSession,
+        {
+          data: {
+            remainingApplicants:
+              curDataCollectionSession.remainingApplicants - 1,
+          },
+        }
+      );
+    }
+
+    // Calling the default core action
+    const { data, meta } = await super.update(ctx);
+
+    return { data, meta };
+  },
 }));
